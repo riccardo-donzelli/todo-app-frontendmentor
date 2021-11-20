7a2e4f0e033e4e814a72a3bb2509586df882422a
riccardo-donzelli <69255523+riccardo-donzelli@users.noreply.github.com>
2021-11-20 16:30:48 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,65 @@
+# Frontend Mentor - Todo app solution
+
+This is a solution to the [Todo app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/todo-app-Su1_KokOW). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Add new todos to the list
+- Mark todos as complete
+- Delete todos from the list
+- Filter by all/active/complete todos
+- Clear all completed todos
+- Toggle light and dark mode
+- **Bonus**: Drag and drop to reorder items on the list
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vanilla JavaScript
+
+### What I learned
+
+I have learned how to drag and drop list elements, how to change icon for a dark theme switch,
+and I came up with a solution to count only the active elements inside the todo list, that change instantly when you add a new element, you cancel and old one or you click on and old one to mark it as "completed".
+I also learned how to set a local storage to keep data after the user closes the browser, so the user will be able to see the todo list as he/she updated it before when he/she will reopen
+the browser later.
+
+
+### Useful resources
+
+- (https://www.youtube.com/watch?v=jfYWwQrtzzY&ab_channel=WebDevSimplified)
+  This helped me for drag and drop elements inside the todo list.
+
+
+## Author
+
+- Website - [riccardo-donzelli](https://github.com/riccardo-donzelli)
+- Frontend Mentor - [@riccardo-donzelli](https://www.frontendmentor.io/profile/riccardo-donzelli)
---
+++ app.js
@@ -0,0 +1,133 @@
+const body = document.body;
+const main = document.getElementById("main");
+const toggle = document.getElementById("theme-toggle");
+const iconTheme = document.getElementById("icon-box");
+const lastLine = document.getElementById("last-line");
+const filtersSection = document.getElementById("filters");
+const filtersLarge = document.querySelector("[data-filter-large]");
+// List & items
+const listItems = document.getElementsByClassName("list-item");
+const listItemsArray = document.querySelectorAll("#list li");
+const draggables = document.querySelectorAll('.draggable');
+let list = document.getElementById("list");
+// Inputs & buttons
+const enterInput = document.getElementById("enter-btn");
+const cancelItem = document.getElementsByClassName("cancelThisItem");
+const clearCompleted = document.getElementById("clear-completed");
+let input = document.getElementById("input-field");
+let counterItems = document.getElementById("number");
+
+// FUNCTIONS
+function themeSwitch() {
+    body.classList.toggle("body-dark");
+    main.classList.toggle("bg-dark");
+    [...listItems, lastLine, filtersSection, input].map(el => el.classList.toggle("list-item-dark"));
+    [...cancelItem].map(el => el.classList.toggle("cancelThisItem-dark"));
+    iconTheme.classList.toggle("sun-icon");
+}
+
+// to submit the input with enter button on keyboard:
+function pressEnter(e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        enterInput.click();
+    }
+}
+
+function counter() {
+    let activeItems = [...listItems].filter(el => !el.className.includes("completed"))
+    counterItems.innerText = activeItems.length;
+}
+counter();
+
+function todo() {
+    let inputValue = input.value;
+    if (inputValue.length > 0) {
+        let newItem = document.createElement("li");
+        newItem.innerHTML = inputValue + 
+            '<span><img src="./images/icon-cross.svg" class="cancelThisItem"></span>';
+        //make the new item draggable adding the attribute:
+        let dragAttribute = document.createAttribute("draggable");
+        attribute.value = "true";
+        newItem.setAttributeNode(dragAttribute);
+        newItem.classList.add("list-item draggable");
+        list.appendChild(newItem);
+    }
+    // reset input field
+    input.value = "";
+    // update counter
+    counter();
+}
+
+function completedTodo(e) {
+    let target = e.target;
+    target.classList.toggle("completed");
+    counter();
+}
+
+function resetInput() {
+    inputValue = "";
+}
+
+function showCancelItem(e) {
+    let target = e.target;
+    let grandchild = target.lastChild.firstChild;
+    grandchild.classList.toggle("show");
+}
+
+function cancelTodo(e) {
+    let target = e.target;
+    let grandparent = target.parentNode.parentNode;
+    list.removeChild(grandparent);
+    counter();
+}
+
+function clearAllCompleted() {
+    [...listItems].map(function(el) {
+        if (el.classList.contains("completed")) {
+            list.removeChild(el);
+        }
+    })
+    counter();
+}
+
+function filter(e) {
+    let target = e.target;
+    if (target.innerText == "Completed") {
+        [...listItems].map(function(el) {
+            if (!el.classList.contains("completed")) {
+                el.classList.add("hide");
+            } else if (el.classList.contains("completed")) {
+                el.classList.remove("hide");
+            }
+        })   
+    }
+    else if (target.innerText == "Active") {
+        [...listItems].map(function(el) {
+            if (el.classList.contains("completed")) {
+                el.classList.add("hide");
+            } else if (!el.classList.contains("completed")) {
+                el.classList.remove("hide");
+            }
+        })
+    }
+    else if (target.innerText == "All") {
+        [...listItems].map(function(el) {
+            el.classList.remove("hide");
+        })
+    }  
+}
+
+// EVENT LISTENERS
+toggle.addEventListener("click", themeSwitch);
+enterInput.addEventListener("click", todo);
+input.addEventListener("keyup", pressEnter);
+list.addEventListener("click", cancelTodo);
+list.addEventListener("click", completedTodo);
+list.addEventListener("mouseover", showCancelItem);
+filtersSection.addEventListener("click", filter);
+filtersLarge.addEventListener("click", filter);
+clearCompleted.addEventListener("click", clearAllCompleted);
+
+// ===========================================================
+// DRAG & DROP FUNCTIONALITY:
